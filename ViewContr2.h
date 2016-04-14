f983a33b47e7c57443879f123a6cab7061f3ae48
Natalia Spiridonova <natusic199797@gmail.com>
2016-04-14 16:43:49 +0300

    Image

---
+++ ViewContr2.h
@@ -0,0 +1,17 @@
+//
+//  ViewContr2.h
+//  TrioFit
+//
+//  Created by Наталья on 09.04.16.
+//  Copyright © 2016 Наталья. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewContr2 : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *mainButton;
+@property (weak, nonatomic) IBOutlet UIButton *libraryButton;
+@property (weak, nonatomic) IBOutlet UIButton *calendarButton;
+@property (weak, nonatomic) IBOutlet UIButton *foodButton;
+
+@end
---
+++ ViewContr3.m
@@ -0,0 +1,50 @@
+//
+//  ViewContr3.m
+//  TrioFit
+//
+//  Created by Наталья on 09.04.16.
+//  Copyright © 2016 Наталья. All rights reserved.
+//
+
+#import "ViewContr3.h"
+#import "ViewContr2.h"
+
+
+@interface ViewContr3 ()
+
+@end
+
+@implementation ViewContr3
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.stepper.wraps = YES;
+    self.stepper.autorepeat = YES;
+    self.counterLabel.text = @"1";
+    self.stepper.minimumValue = 1;
+    
+    self.stepper.maximumValue = 7;
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)stepperValueChanged:(id)sender {
+    NSUInteger value = _stepper.value;
+    _counterLabel.text = [NSString stringWithFormat:@"%d",value];
+}
+@end
